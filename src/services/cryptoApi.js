132df362512bd4c9b552c39cd231d46b088d2e09
Bryan_Palad <paladbryanj@gmail.com>
2022-03-27 17:37:28 +0800

    add_env_file

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-    'X-RapidAPI-Key': '9f3c4f580emshc2def8695f26f06p16c5c4jsn6ea225972f4e'
+    'X-RapidAPI-Key': process.env.REACT_APP_KEY
 }
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
